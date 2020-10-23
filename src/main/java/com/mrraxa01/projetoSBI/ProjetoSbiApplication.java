8966027d4fe95f0a837ea433e2eb95749f666a85
mrraxa01 <mrraxa01@gmail.com>
2020-10-23 09:43:43 -0300

    Projeto Criado

---
+++ src/main/java/com/mrraxa01/projetoSBI/ProjetoSbiApplication.java
@@ -0,0 +1,13 @@
+package com.mrraxa01.projetoSBI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoSbiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoSbiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mrraxa01/projetoSBI/ProjetoSbiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mrraxa01.projetoSBI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoSbiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
