5e91366ff6372e2bd13be339e3a64a8bdbd19b57
mrraxa01 <mrraxa01@gmail.com>
2020-10-25 19:42:27 -0300

    Criando operação de instanciação

--- src/main/java/com/mrraxa01/projetoSBI/ProjetoSbiApplication.java
+++ src/main/java/com/mrraxa01/projetoSBI/ProjetoSbiApplication.java
@@ -1,13 +1,33 @@
 package com.mrraxa01.projetoSBI;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class ProjetoSbiApplication {
+import com.mrraxa01.projetoSBI.domain.Categoria;
+import com.mrraxa01.projetoSBI.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class ProjetoSbiApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 	public static void main(String[] args) {
 		SpringApplication.run(ProjetoSbiApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+	
+		categoriaRepository.deleteAll();
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+		
+		
 	}
 
 }
