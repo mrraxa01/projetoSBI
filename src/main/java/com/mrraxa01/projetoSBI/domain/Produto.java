785954027e33787178529eea37fdd0e641c3ca4e
mrraxa01 <mrraxa01@gmail.com>
2020-10-26 18:53:55 -0300

    Ajustes no endpoint/categoria/{id}

--- src/main/java/com/mrraxa01/projetoSBI/domain/Produto.java
+++ src/main/java/com/mrraxa01/projetoSBI/domain/Produto.java
@@ -12,11 +12,14 @@ import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
 @Entity
 public class Produto implements Serializable{
 
 	private static final long serialVersionUID = 1L;
 
+	@JsonBackReference
 	@ManyToMany
 	@JoinTable(name = "PRODUTO_CATEGORIA",
 				joinColumns = @JoinColumn (name = "produto_id"),
