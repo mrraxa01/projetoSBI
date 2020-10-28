ab7eec7f11acc1c7b8d08d59b8ca4f3a37ae9606
mrraxa01 <mrraxa01@gmail.com>
2020-10-28 19:21:03 -0300

    Atualização utilizando somente JsonIgnore

--- src/main/java/com/mrraxa01/projetoSBI/domain/Pagamento.java
+++ src/main/java/com/mrraxa01/projetoSBI/domain/Pagamento.java
@@ -10,7 +10,7 @@ import javax.persistence.JoinColumn;
 import javax.persistence.MapsId;
 import javax.persistence.OneToOne;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.mrraxa01.projetoSBI.domain.enums.EstadoPagamento;
 
 @Entity
@@ -24,7 +24,7 @@ public abstract class  Pagamento implements Serializable {
 	private Integer id;
 	private Integer estado;//para que o enum retorne 1 cod ao invés de string, deve-se declarar como Integer. Observar o constr., get e set 
 	
-	@JsonBackReference
+	@JsonIgnore
 	@OneToOne
 	@JoinColumn(name = "pedido_id")
 	@MapsId
