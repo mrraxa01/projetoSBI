49d97b1df73b796da69b781da013bde991eec620
mrraxa01 <mrraxa01@gmail.com>
2020-10-28 21:42:56 -0300

    Deletando uma categoria com DELETE

--- src/main/java/com/mrraxa01/projetoSBI/services/CategoriaService.java
+++ src/main/java/com/mrraxa01/projetoSBI/services/CategoriaService.java
@@ -3,10 +3,12 @@ package com.mrraxa01.projetoSBI.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.mrraxa01.projetoSBI.domain.Categoria;
 import com.mrraxa01.projetoSBI.repositories.CategoriaRepository;
+import com.mrraxa01.projetoSBI.services.exceptions.DataIntegrityException;
 import com.mrraxa01.projetoSBI.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -29,4 +31,14 @@ public class CategoriaService {
 		find(obj.getId());//método implementado acima, busca o id no BD e caso não exista já lança excessão.
 		return repo.save(obj);
 	}
+	
+	public void delete(Integer id) {
+		find(id);
+		try {
+		repo.deleteById(id);
+	}catch (DataIntegrityViolationException e){
+		throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos");
+		
+	}
+		}
 }
---
+++ src/main/java/com/mrraxa01/projetoSBI/services/exceptions/DataIntegrityException.java
@@ -0,0 +1,13 @@
+package com.mrraxa01.projetoSBI.services.exceptions;
+
+public class DataIntegrityException extends RuntimeException{
+
+	private static final long serialVersionUID = 1L;
+
+	public DataIntegrityException(String msg) {
+		super(msg);
+	}
+	public DataIntegrityException(String msg, Throwable cause) {
+		super (msg, cause);
+	}
+}
