fca2273bb83788109a927bfa743530153c1108a4
mrraxa01 <mrraxa01@gmail.com>
2020-10-29 22:16:16 -0300

    Paginação com parametros opcionais na requisição

--- src/main/java/com/mrraxa01/projetoSBI/ProjetoSbiApplication.java
+++ src/main/java/com/mrraxa01/projetoSBI/ProjetoSbiApplication.java
@@ -63,6 +63,12 @@ public class ProjetoSbiApplication implements CommandLineRunner {
 		//categoriaRepository.deleteAll();
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritório");
+		Categoria cat3 = new Categoria(null, "Cama, mesa e banho");
+		Categoria cat4 = new Categoria(null, "Eletrônicos");
+		Categoria cat5 = new Categoria(null, "Jardinagem");
+		Categoria cat6 = new Categoria(null, "Decoração");
+		Categoria cat7 = new Categoria(null, "Perfumaria");
+		
 		
 		
 	Produto p1 = new Produto(null, "Computador", 2000.00);
@@ -76,7 +82,7 @@ public class ProjetoSbiApplication implements CommandLineRunner {
 	p2.getCategorias().addAll(Arrays.asList(cat1,cat2));
 	p3.getCategorias().addAll(Arrays.asList(cat1));
 
-	categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+	categoriaRepository.saveAll(Arrays.asList(cat1,cat2,cat3,cat4,cat5,cat6,cat7));
 	produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
 	
 	Estado est1 = new Estado(null, "Minas Gerais");
