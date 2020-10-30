5a516af38cc9463403356e6bbf2c523214f4c5a1
mrraxa01 <mrraxa01@gmail.com>
2020-10-29 21:08:43 -0300

    Listando todas as categorias com DTO

--- src/main/java/com/mrraxa01/projetoSBI/services/CategoriaService.java
+++ src/main/java/com/mrraxa01/projetoSBI/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.mrraxa01.projetoSBI.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -41,4 +42,9 @@ public class CategoriaService {
 		
 	}
 		}
+	
+	public List<Categoria> findAll() {
+		List<Categoria> list = repo.findAll();
+		return list;
+}
 }
