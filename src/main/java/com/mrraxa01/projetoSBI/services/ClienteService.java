48bfd43847d45f96794528ed309352262451d558
mrraxa01 <mrraxa01@gmail.com>
2020-11-02 15:22:32 -0300

    Ajustando e testando a deleção de Cliente

--- src/main/java/com/mrraxa01/projetoSBI/services/ClienteService.java
+++ src/main/java/com/mrraxa01/projetoSBI/services/ClienteService.java
@@ -3,6 +3,8 @@ package com.mrraxa01.projetoSBI.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
@@ -35,6 +37,7 @@ public class ClienteService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " +id + ", Tipo: " + Cliente.class.getName()));
 	}
 
+	@Transactional
 	public Cliente insert(Cliente obj) {
 		obj.setId(null);
 		obj = repo.save(obj);
