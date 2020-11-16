3111df877adee257944bb79d73422105d4343c90
mrraxa01 <mrraxa01@gmail.com>
2020-11-15 21:51:25 -0300

    Implementando autorização - CORRIGIDOgit add .git add .

--- src/main/java/com/mrraxa01/projetoSBI/config/SecurityConfig.java
+++ src/main/java/com/mrraxa01/projetoSBI/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.mrraxa01.projetoSBI.config;
 
+/*
 
 import java.util.Arrays;
 
@@ -20,10 +21,92 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.mrraxa01.projetoSBI.security.JWTAuthenticationFilter;
+import com.mrraxa01.projetoSBI.security.JWTAuthorizationFilter;
 import com.mrraxa01.projetoSBI.security.JWTUtil;
 
 @Configuration
 @EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Autowired
+	private UserDetailsService userDetailsService;
+	
+	@Autowired
+    private Environment env;
+	
+	@Autowired
+	private JWTUtil jwtUtil;
+	
+	private static final String[] PUBLIC_MATCHERS = {
+			"/h2-console/**"
+	};
+
+	private static final String[] PUBLIC_MATCHERS_GET = {
+			"/produtos/**",
+			"/categorias/**",
+			"/clientes/**"
+	};
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		
+		if (Arrays.asList(env.getActiveProfiles()).contains("test")) {
+            http.headers().frameOptions().disable();
+        }
+		
+		http.cors().and().csrf().disable();
+		http.authorizeRequests()
+			.antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
+			.antMatchers(PUBLIC_MATCHERS).permitAll()
+			.anyRequest().authenticated();
+		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
+		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+	}
+	
+	@Override
+	public void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder());
+	}
+	
+	@Bean
+	CorsConfigurationSource corsConfigurationSource() {
+		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+		return source;
+	}
+	
+	@Bean
+	public BCryptPasswordEncoder bCryptPasswordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+}
+*/
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import com.mrraxa01.projetoSBI.security.JWTAuthenticationFilter;
+import com.mrraxa01.projetoSBI.security.JWTAuthorizationFilter;
+import com.mrraxa01.projetoSBI.security.JWTUtil;
+
+@Configuration
+@EnableWebSecurity
+
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 @Autowired
@@ -34,18 +117,22 @@ private JWTUtil jwtUtil;
 	//quem estiver no vetor abaixo estará liberado:
 @Autowired
 private UserDetailsService userDetailsService;
-	
-	private static final String[] PUBLIC_MATCHES = {
+
+
+
+
+private static final String[] PUBLIC_MATCHERS = {
 			"/h2-console/**"
 			
 	};
 	//os endpoints só terão acesso para leitura
-	private static final String[] PUBLIC_MATCHES_GET = {
+	private static final String[] PUBLIC_MATCHERS_GET = {
 			"/produtos/**",
-			"/categorias/**"
+			"/categorias/**",
+			"/clientes/**"
 			
 	};
-	
+
 	@Override
 	protected void configure(HttpSecurity http) throws Exception{
 		//Esse if só se aplica ao perfil de teste
@@ -56,13 +143,18 @@ private UserDetailsService userDetailsService;
 		//o metodo csrf = disable desabilita seguranca. deve ser habilitado qdo se salva senha em sessões
 		http.cors().and().csrf().disable();
 		http.authorizeRequests()
-		.antMatchers(HttpMethod.GET, PUBLIC_MATCHES_GET).permitAll()
-		.antMatchers(PUBLIC_MATCHES).permitAll()
+		.antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
+		.antMatchers(PUBLIC_MATCHERS).permitAll()
 		.anyRequest().authenticated();
-		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));		
+		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));	
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));	
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
+
+
 	
+
+
 	@Override
 	public void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder());
--- src/main/java/com/mrraxa01/projetoSBI/security/JWTUtil.java
+++ src/main/java/com/mrraxa01/projetoSBI/security/JWTUtil.java
@@ -21,7 +21,7 @@ public class JWTUtil {
 	public String generateToken(String username) {
 		return Jwts.builder()
 				.setSubject(username)
-				.setExpiration(new Date(System.currentTimeMillis()))
+				.setExpiration(new Date(System.currentTimeMillis()+ expiration))
 				.signWith(SignatureAlgorithm.HS512, secret.getBytes())
 				.compact();
 				
